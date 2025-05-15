edf280eb317c614469dee3f3ece09ee4576c1e7b
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-15 16:39:09 -0300

    Prepare for API

--- MoneyMap/Controllers/HomeController.cs
+++ MoneyMap/Controllers/HomeController.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace MoneyMap.Controllers
 {
+    [Route("api/v1/[controller]")]
+    [ApiController]
     public class HomeController : Controller
     {
 
-        public HomeController()
+        [HttpGet("GetApiForTest")]
+        public async Task<IActionResult> GetApiForTest()
         {
-        }
-
-        public IActionResult Index()
-        {
-            return View();
+            return Ok();
         }
     }
 }
--- MoneyMap/Program.cs
+++ MoneyMap/Program.cs
@@ -1,27 +1,75 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add services to the container.
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//builder.Services.AddAuthentication("ApiKey")
+//        .AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>("ApiKey", null);
+
+
+// Register Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoneyMap API", Version = "v1" });
+
+    // Configure API Key Authentication
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "API Key needed to access the endpoints.",
+        In = ParameterLocation.Header,
+        Name = "ApiKey",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "ApiKeyScheme"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                },
+                Scheme = "ApiKeyScheme",
+                Name = "ApiKey",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+//if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoneyMap API V1");
+    });
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
