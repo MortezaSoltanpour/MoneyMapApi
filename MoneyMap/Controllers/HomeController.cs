ce17db0aa5cc6604fa2a11118aa6bf5dbf8fabe2
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-15 16:29:40 -0300

    Add project files.

---
+++ MoneyMap/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyMap.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
