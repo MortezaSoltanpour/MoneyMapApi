09311d4ad54bff86c6ca2b863a94eb7073509633
MortezaSoltanpour <morteza.cs@gmail.com>
2025-05-15 16:51:43 -0300

    Add user Entity

---
+++ MoneyMap/Models/Configurations/UsersConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyMap.Models.Entities;
+
+namespace MoneyMap.Models.Configurations
+{
+    public class UsersConfigurations : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builder)
+        {
+            builder.HasKey(p => p.IdUser);
+            builder.Property(p => p.DateRegistered).IsRequired().HasDefaultValueSql("GETDATE()");
+            builder.Property(p => p.IsDeleted).HasDefaultValue(false).IsRequired();
+
+            builder.Property(p => p.Email).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Password).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Fullname).IsRequired().HasMaxLength(100);
+        }
+    }
+}
---
+++ MoneyMap/Models/Entities/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoneyMap.Models.Entities
+{
+    public class Entity
+    {
+        [Display(Name = "Date")]
+        public DateTime DateRegistered { get; set; }
+        public bool IsDeleted { get; set; }
+
+        [NotMapped]
+        public string? ImagePath1 { get; set; }
+
+        [NotMapped]
+        public string? ImagePath2 { get; set; }
+
+        [NotMapped]
+        public string? ImagePath3 { get; set; }
+
+        [NotMapped]
+        public string? ImagePath4 { get; set; }
+
+        [NotMapped]
+        public DateTime LastUpdated { get; set; } = DateTime.Now;
+    }
+}
